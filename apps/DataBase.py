f51c8e2561b575c4a8388c09a891d19e767df501
nikolayivan <61462702+nikolayivan@users.noreply.github.com>
2022-03-15 19:46:43 +0300

    Update DataBase.py

--- apps/DataBase.py
+++ apps/DataBase.py
@@ -13,142 +13,233 @@ def app():
 #     FileName = '/Таблица Данных.xlsx'
 
     @st.experimental_memo
-    def LoadDataBase(FolderPath,FileName):
-        con = sqlite3.connect(FolderPath + FileName)
+    def LoadDataBase(FolderPath,FileName, ColOption):
+        con = sqlite3.connect(r'C:\Users\testingcenter\Documents\StreamlitApps\006_DataBase\data\DataBase.db')
         cur = con.cursor()
         DataBase_df = pd.read_sql('SELECT * FROM data', con, parse_dates=['Дата'])
         con.close()
         # DataBase_df = pd.read_excel(FolderPath + FileName, header = 0, parse_dates=['Дата'])
         DataBase_df["Дата"] = DataBase_df["Дата"].dt.date
+
+        if ColOption:
+            nan_value = float("NaN")
+            DataBase_df.replace("", nan_value, inplace=True)
+            DataBase_df.dropna(how='all', axis=1, inplace=True)
         return DataBase_df
-
-    def DeleteRow(ID):
-        con = sqlite3.connect(FolderPath + FileName)
-        con.execute("DELETE FROM data WHERE id=(%s)" % ID)
-        con.commit()
-        con.close()
-        st.success('Ряд успешно удален!')
-
-    DataBase_df = LoadDataBase(FolderPath,FileName)
-    # DataBase_df.sort_values('Дата', inplace=True, ignore_index=True)
-    con1 = st.container()
 
     st.write('Обновить таблицу:')
     if st.button('Обновить'):
         LoadDataBase.clear()
-        DataBase_df = LoadDataBase(FolderPath,FileName)
-        # DataBase_df.sort_values('Дата', inplace=True, ignore_index=True)
+        DataBase_df = LoadDataBase(FolderPath,FileName,ColOption)
+        DataBase_df.sort_values('Дата', inplace=True, ignore_index=True)
         # st.session_state['DataBase_df'] = DataBase_df.to_dict()
 
-    con1.write(f"Общее кол-во записей: `{DataBase_df.shape[0]}`")
-    con1.write(f"Общее кол-во столбцов: `{DataBase_df.shape[1]}`" )
+    con_1 = st.container()
+    # ColOption = st.checkbox('Исключить пустые столбцы')
+    ColOption = False
+    DataBase_df = LoadDataBase(FolderPath,FileName,ColOption)
+    DataBase_df.sort_values('Дата', inplace=True, ignore_index=True)
 
-    # con1.write('FFF `{}` ')
+    st.write("Общее кол-во записей: ", DataBase_df.shape[0] )
+    st.write("Общее кол-во столбцов: ", DataBase_df.shape[1] )
+
     col1, _ = st.columns([1,5])
-
     Number = col1.number_input('Укажите кол-во строк для отображения:', value=5)
-
     # st.write('Показать последние 10 записей из общей таблицы:')
     with st.expander("Посмотреть таблицу"):
         st.dataframe(DataBase_df.tail(Number))
 
-    col1, _ = st.columns([1,5])
-    ID = col1.number_input('Укажите id для удаления:', value=0)
-    if st.button('Удалить'):
-        DeleteRow(ID)
+    Plot_df = DataBase_df
+    Plot_df.sort_values('Дата', inplace=True)
 
-    con_1 = st.container()
-    # ColOption = st.checkbox('Исключить пустые столбцы')
-    uploaded_file = st.file_uploader("Загрузить файл")
+    with st.form("form_2"):
+        st.write('Анализ данных по выбранному параметру:')
+        con_2 = st.container()
 
-    if uploaded_file is not None:
+        if st.checkbox('Выбрать все ЭОБ'):
+            RowOptionList = list(dict.fromkeys(Plot_df['Данные ЭОБ: Зав. номер трансформатора'].tolist()))
+            RowOption = RowOptionList
+        else:
+            RowOption = list(dict.fromkeys(Plot_df['Данные ЭОБ: Зав. номер трансформатора'].tolist()))
+            RowOptionList = None
 
-        with st.form("form_1"):
+        con_3 = st.container()
 
-            Date = st.date_input("Укажите дату",datetime.today())
-            if not Date:
-                st.warning('Укажите Дату!')
+        if st.checkbox('Выбрать все параметры'):
+            ColOptionList = Plot_df.columns.tolist()
+            ColOptions = ColOptionList
+        else:
+            ColOptionList = None
+            ColOptions = [
+            'Диагностические параметры (EOM Фаза А): Контраст',
+            'Диагностические параметры (EOM Фаза А): Umod',
+            'Диагностические параметры (EOM Фаза А): Max. ADC',
+            'Диагностические параметры (EOM Фаза В): Контраст',
+            'Диагностические параметры (EOM Фаза В): Umod',
+            'Диагностические параметры (EOM Фаза В): Max. ADC',
+            'Диагностические параметры (EOM Фаза С): Контраст',
+            'Диагностические параметры (EOM Фаза С): Umod',
+            'Диагностические параметры (EOM Фаза С): Max. ADC',
+            'Диагностические параметры (EOM Фаза С): Tin',
+            'Лазерный излучатель: Ток Лазерного Излучателя']
 
-            df0 = pd.read_csv(uploaded_file, sep=';', header=None, delimiter = None, names=['Параметр','Значение'], encoding="cp1251", na_filter=True, skip_blank_lines=True)
-            df0.drop_duplicates(subset = ['Параметр'], keep = 'last', inplace=True)
+        AddColOptions = [
+        'Дата',
+        'Данные ЭОБ: Зав. номер трансформатора',
+        'Оптические параметры (EOM Фаза А): Част. модуляции',
+        'Оптические параметры (EOM Фаза В): Част. модуляции',
+        'Оптические параметры (EOM Фаза С): Част. модуляции',
+        ]
 
-            row_1_s = pd.Series({'Параметр': 'Дата', 'Значение': str(Date)})
-            row_1_df = pd.DataFrame([row_1_s])
-            df0 = pd.concat([row_1_df, df0], ignore_index=True)
+        con_4 = st.container()
+        Rows = con_2.multiselect('Выбрать ЭОБ:', options=RowOption , default = RowOptionList)
+        MainColumns = con_3.multiselect('Выбрать основные параметры:', options=ColOptions, default = ColOptionList)
+        AddColumns = con_4.multiselect('Выбрать дополнительные параметры:', options=AddColOptions[3:])
 
-            Columns = df0.iloc[:,0].dropna().tolist()
-            Columns = [Col for Col in Columns if Col[0] != "=" if Col[0] != ":" if Col[0] != ""]
-            df1 = df0.loc[df0.iloc[:,0].isin(Columns)].reset_index(drop=True)
+        Columns = AddColOptions[:3] + MainColumns + AddColumns
 
-            for row, name in enumerate(df1['Параметр']):
-                df1.iloc[row,0] = ' '.join(name.split())
+        Columns = list(dict.fromkeys(Columns))
 
-            SN = df1.loc[ df1['Параметр'] == 'Данные ЭОБ: Зав. номер трансформатора', 'Значение'].reset_index(drop=True)[0]
+        submitted = st.form_submit_button("Ввод")
 
-            # if len(SN) < 2:
-            SN = st.text_input("Укажите серийный номер ЭОБ", value=SN)
-            if len(SN) < 2:
-            # if not SN:
-                st.warning('Укажите серийный номер ЭОБ!')
+        if submitted:
+            pass
+        else:
+            st.stop()
+
+    if not Rows:
+        st.warning('Выберите ряды!')
+        st.stop()
+
+    if not Columns:
+        st.warning('Выберите столбцы!')
+        st.stop()
+
+    selected_df = Plot_df.loc[Plot_df['Данные ЭОБ: Зав. номер трансформатора'].isin(Rows), Columns]
+    # st.write(selected_df.info())
+    # selected_df = Plot_df
+
+    datetimes = pd.to_datetime(selected_df["Дата"])
+    selected_df["Дата"] = datetimes
+    selected_df.sort_values('Дата', inplace=True)
+
+    # st.line_chart(selected_df)
+    st.header('')
+    st.header('Результаты в абсолютных единицах')
+
+    with st.expander("Посмотреть результирующую таблицу"):
+        st.dataframe(selected_df)
+
+    if Rows:
+        if MainColumns:
+            df = selected_df[selected_df['Оптические параметры (EOM Фаза А): Част. модуляции'] < 50 ]
+            if not df.empty:
+                # st.write(df)
+                st.subheader('Частота модуляции 38 кГц')
+                fig = px.scatter(df, x="Дата", y=MainColumns, hover_name=df['Данные ЭОБ: Зав. номер трансформатора'])
+                fig.update_layout(legend_title_text=None,
+                        legend=dict(
+                        orientation="h",
+                        yanchor="bottom",
+                        y=1.02,
+                        xanchor="right",
+                        x=1))
+                st.plotly_chart(fig, use_container_width=True)
+
+            df = selected_df[selected_df['Оптические параметры (EOM Фаза А): Част. модуляции'] > 50 ]
+            if not df.empty:
+                # st.write(df)
+                st.subheader('Частота модуляции 66 кГц')
+                fig = px.scatter(df, x="Дата", y=MainColumns, hover_name=df['Данные ЭОБ: Зав. номер трансформатора'])
+                fig.update_layout(legend_title_text=None,
+                        legend=dict(
+                        orientation="h",
+                        yanchor="bottom",
+                        y=1.02,
+                        xanchor="right",
+                        x=1))
+                st.plotly_chart(fig, use_container_width=True)
+
+            # marginal_y="violin",
+            # marginal_x="box", trendline="ols", template="simple_white")
+
+            # fig3 = px.scatter(grouped_df, y=Columns[1:])
+            # st.plotly_chart(fig3, use_container_width=True)
+            #
+            # fig3 = px.box(grouped_df, y=Columns[1:], points="all")
+            # st.plotly_chart(fig3, use_container_width=True)
+            #
+            # fig3 = px.violin(grouped_df, y=Columns[1:], points="all")
+            # st.plotly_chart(fig3, use_container_width=True)
+
+
+    # st.stop()
+
+    selected_df_5 = pd.DataFrame()
+
+    for Row in Rows:
+        selected_df_6  = pd.DataFrame()
+        selected_df_2  = selected_df.loc[selected_df['Данные ЭОБ: Зав. номер трансформатора'].isin([Row]), :]
+
+        # st.write(selected_df_2)
+        # st.stop()
+
+        if type(selected_df_2) == type(pd.Series([1,2,3])):
+            selected_df_2  = selected_df_2.to_frame().T
+            # selected_df_2 = selected_df_2.reset_index()
+            # selected_df_2.rename(columns={'index': 'Данные ЭОБ: Зав. номер трансформатора'}, inplace=True)
+        # else:
+        #     selected_df_2 = selected_df_2.reset_index()
+
+        for Ind, Col in enumerate(selected_df_2.columns):
+            Series = selected_df_2.loc[:,Col]
+            if Col == 'Дата':
+                selected_df_6[Col] = Series.sub(Series.iloc[0]).dt.days
+                # selected_df_6[Col] = selected_df_2.loc[:,Col].sub(selected_df_2.iloc[0,Ind]).dt.days
+            elif Col == 'Данные ЭОБ: Зав. номер трансформатора':
+                selected_df_6[Col] = selected_df_2[Col]
+            elif Col == 'Оптические параметры (EOM Фаза А): Част. модуляции':
+                selected_df_6[Col] = selected_df_2[Col]
             else:
-                df1.loc[ df1['Параметр'] == 'Данные ЭОБ: Зав. номер трансформатора', 'Значение'] = SN
+                for i in Series.index:
+                    if not pd.isna(Series[i]):
+                        selected_df_6[Col] = Series.div(Series[i])
+                        break
+                    # else:
+                    #     selected_df_6[Col] = Series.div(Series.iloc[1])
 
-            with st.expander("Посмотреть загруженную таблицу"):
-                st.write(df1)
+        selected_df_5 = pd.concat([selected_df_5, selected_df_6], ignore_index=False)
+        # selected_df_5.sort_values('Дата', inplace=True)
 
-            final_df = df1.T
-            final_df.columns = final_df.iloc[0]
-            final_df = final_df.drop('Параметр').reset_index(drop=True)
 
-            result = pd.concat([DataBase_df, final_df], ignore_index=True, sort=False)
+    st.header('')
+    st.header('Результаты в относительных единицах')
 
-            df_to_save = result.iloc[-1]
-            df_to_save = df_to_save.to_frame().T
+    with st.expander('Показать таблицу'):
+        st.write(selected_df_5)
 
-                # with st.expander("Посмотреть таблицу для записи"):
-                #     st.write(df_to_save)
-            st.write('Записать в общую таблицу:')
-            submitted_1 = st.form_submit_button('Записать')
+    df = selected_df_5[selected_df_5['Оптические параметры (EOM Фаза А): Част. модуляции'] < 50]
+    if not df.empty:
+        st.subheader('Частота модуляции 38 кГц')
+        fig2 = px.scatter(df, x='Дата', y=MainColumns, hover_name='Данные ЭОБ: Зав. номер трансформатора')
+        fig2.update_layout(legend_title_text=None,
+                legend=dict(
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1))
+        st.plotly_chart(fig2, use_container_width=True)
 
-            if submitted_1:
-                if uploaded_file is not None:
-                    con = sqlite3.connect(FolderPath + FileName)
-                    df_to_save["Дата"] = df_to_save["Дата"].astype(str)
-                    df_to_save.replace(np.nan, None, inplace=True)
-                    df_to_save.replace(' ', None, inplace=True)
-                    to_save = tuple(df_to_save.iloc[0,:])
-                    row_value_markers = ','.join(['?']*len(to_save))
-                    con.execute("INSERT INTO data VALUES (%s)" % row_value_markers, to_save)
-                    con.commit()
-                    con.close()
-                    # append_df_to_excel(FolderPath + FileName, df_to_save, sheet_name='Sheet1',header=0, index=False)
-                    # st.success('Данные успешно записаны!')
-                    # with pd.ExcelWriter(FolderPath + FileName, mode="a", engine="openpyxl", if_sheet_exists="overlay",) as writer:
-                    #     df_to_save.to_excel(writer, sheet_name="Sheet1", startrow=writer.sheets['Sheet1'].max_row, index = False,header= False)
-                    st.success('Данные успешно записаны!')
-                else:
-                    st.warning("Файл не загружен! Загрузите файл...")
-
-        # final_df.to_excel(FolderPath + '\output.xlsx', sheet_name='Sheet1', index = False)
-
-        if st.checkbox('Поиск данных'):
-            with st.form("form_2"):
-                st.write('Выбрать определенный набор данных из загруженной таблицы для просмотра:')
-                # if st.checkbox('Выбрать определенный набор данных из загруженной таблицы для просмотра:'):
-                Rows = st.multiselect('Выбрать ряды', options=df1.iloc[:,0])
-
-                submitted = st.form_submit_button("Вывести данные")
-
-                if submitted:
-                    selected_df = df1.loc[df1['Параметр'].isin(Rows)]
-
-            if Rows:
-                st.write(selected_df)
-
-    if not DataBase_df.empty:
-        # st.write('Сохранить на локальный диск:')
-        st.download_button(
-                label="Скачать CSV",
-                data=DataBase_df.to_csv().encode('utf-8'),
-                file_name='Таблица Данных.csv',
-                mime='text/csv')
+    df = selected_df_5[selected_df_5['Оптические параметры (EOM Фаза А): Част. модуляции'] > 50]
+    if not df.empty:
+        st.subheader('Частота модуляции 66 кГц')
+        fig2 = px.scatter(df, x='Дата', y=MainColumns, hover_name='Данные ЭОБ: Зав. номер трансформатора')
+        fig2.update_layout(legend_title_text=None,
+                legend=dict(
+                orientation="h",
+                yanchor="bottom",
+                y=1.02,
+                xanchor="right",
+                x=1))
+        st.plotly_chart(fig2, use_container_width=True)
