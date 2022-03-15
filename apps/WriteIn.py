216873a6f9b03aaf76c68e7b16594a51c594b186
nikolayivan <61462702+nikolayivan@users.noreply.github.com>
2022-03-15 19:45:40 +0300

    Update WriteIn.py

--- apps/WriteIn.py
+++ apps/WriteIn.py
@@ -8,6 +8,7 @@ from datetime import datetime
 def app():
     st.title('Система сбора данных и мониторинг 📈')
     st.sidebar.write('')
+#     st.sidebar.info('About: \n This is a demo version of web application designed to recode and analyse parameters from EPU. All rights belongs to JSC Profotech.')
     FolderPath = r'./data'
     FileName = '/DataBase.db'
 #     FileName = '/Таблица Данных.xlsx'
