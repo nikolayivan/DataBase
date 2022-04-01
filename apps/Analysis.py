124365d567a11e5b7c3c0eebaea75002d0a3798a
nikolayivan <61462702+nikolayivan@users.noreply.github.com>
2022-04-01 16:55:05 +0300

    Update Analysis.py

--- apps/Analysis.py
+++ apps/Analysis.py
@@ -1,18 +1,18 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import os
-import plotly.express as px
-import plotly.graph_objects as go
-import folium
+# import pandas as pd
+# import numpy as np
+# # import os
+# import plotly.express as px
+# import plotly.graph_objects as go
+# import folium
 
-from collections import Counter
-from streamlit_folium import folium_static
-from streamlit_echarts import st_echarts
-from PIL import Image
-from datetime import datetime
-from st_aggrid import AgGrid, GridUpdateMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
+# from collections import Counter
+# from streamlit_folium import folium_static
+# from streamlit_echarts import st_echarts
+# from PIL import Image
+# from datetime import datetime
+# from st_aggrid import AgGrid, GridUpdateMode
+# from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 def app():
     # st.sidebar.subheader('Описание:')
